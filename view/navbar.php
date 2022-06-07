88fad178d3ca5b764d4294a23415e45e4b4a1dd5
IFTE-13 <mariamnoor1290@gmail.com>
2022-06-07 18:59:13 +0600

    Update 06/07/2022

--- view/navbar.php
+++ view/navbar.php
@@ -7,14 +7,17 @@
         <table>
             <tr>
                 <td>
+                    <img src="../utilities/logo.png" alt="company logo">
+                </td>
+                <td>
                     <h1><b>PHARMACY'24</b></h1>
                 </td>
                 <td><sub>Get your medicine 24/7</sub></td>
-                <td><small><sub>_______________________________________________________________________________________________________________________________________________</sub></small></td>
-                <td><h3><a href="http://localhost/Pharmacy24/view/home.php">Home</a></h3></td>
-                <td><h3><a href="http://localhost/Pharmacy24/view/login.php">Login</a></h3></td>
-                <td><h3><a href="http://localhost/Pharmacy24/view/about.php">About</a></h3></td>
-                <td><h3><a href="http://localhost/Pharmacy24/view/contact.php">Contact</a></h2></td>
+                <td><small><sub>_____________________________________________________________________________________________________________________________________</sub></small></td>
+                <td><h3><a href="../view/home.php">Home</a></h3></td>
+                <td><h3><a href="../view/login.php">Login</a></h3></td>
+                <td><h3><a href="../view/about.php">About</a></h3></td>
+                <td><h3><a href="../view/contact.php">Contact</a></h2></td>
             </tr>
         </table>
     </section>
