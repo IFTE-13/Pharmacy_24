3fe67755e82058af560a0fb4d24d74cc2f506d51
IFTE-13 <mariamnoor1290@gmail.com>
2022-06-17 22:14:56 +0600

    Admin profile version 2.3

--- view/admin/adminRemoveUser.php
+++ view/admin/adminRemoveUser.php
@@ -1,17 +1,20 @@
 <?php
     include("adminSideBar.php");
-    include("../../control/adminUserAdd.php");
+    include("../../control/adminUserRemove.php");
     
 ?>
 
 <html>
+    <head>
+        <title>Remove User</title>
+    </head>
     <body>
         <table width="100%">
             <tr>
                 <td>
                     <a href="../admin/adminUser.php">| Add User |</a>
                     <a href="../admin/adminRemoveUser.php">Remove User |</a>
-                    <a href="block.php">| Block User |</a>
+                    <a href="block.php">Block User |</a>
                     <a href="block.php">Complains |</a>
                 </td>
             </tr>
@@ -25,7 +28,7 @@
         <form action="" method="POST">
                     <table>
                         <tr>
-                            <td><h1><u>Add User</u></h1></td>
+                            <td><h1><u>Remove User</u></h1></td>
                         </tr>
         <tr>
             <td><h3>Name:</h3></td>
@@ -36,30 +39,9 @@
             <td><input type="email" name="email" id=""></td>
         </tr>
         <tr>
-            <td><h3>Username:</h3></td>
-            <td><input type="text" name="username" id=""></td>
-        </tr>
-        <tr>
-            <td><h3>Password:</h3></td>
-            <td><input type="password" name="password" id="" placeholder="00000000" disabled></td>
-        </tr>
-        <tr>
-            <td><h3>Gender:</h3></td>
-            <td>
-                <input type="radio" name="gender" id="" value="male">
-                <label for="gender">Male</label>
-                <input type="radio" name="gender" id="" value="female">
-                <label for="gender">Female</label>
-            </td>
-        </tr>
-        <tr>
-            <td><h3>Date of Birth:</h3></td>
-            <td><input type="date" name="dob" id=""></td>
-        </tr>
-        <tr>
             <td></td>
             <td>
-                <input type="submit" value="Submit" name="adminAddUser">
+                <input type="submit" value="Submit" name="adminRemoveUser">
                 <input type="reset" value="Reset">
             </td>
         </tr>
@@ -67,7 +49,7 @@
             <td></td>
         <td>
         <?php
-                    echo $userAdded;
+                    echo $userRemoved;
                 ?>
         </td>
         </tr>
