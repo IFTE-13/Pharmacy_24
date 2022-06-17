cb497689a15287f456a3e67f6c3e5041d43239ec
IFTE-13 <mariamnoor1290@gmail.com>
2022-06-18 02:25:02 +0600

    Admin Seller CRUD operation complete

---
+++ control/admin/seller/adminSellerBlock.php
@@ -0,0 +1,28 @@
+<?php
+    $userBlocked = "";
+    $new_data = "";
+    $verifyAdmin = file_get_contents("http://localhost/Pharmacy24/view/admin/sellerData.json", true);
+    $data =  json_decode($verifyAdmin);
+
+    if(isset($_POST["adminBlockSeller"])){
+        if(empty($_REQUEST["name"])||empty($_REQUEST["email"])){
+            $userBlocked = 'None of the fields cannot be empty';
+        }
+        else{
+            foreach($data as $key=>$value){
+                    if($value->name == $_REQUEST["name"] && $value->email == $_REQUEST["email"]){
+                        $value->status = $_REQUEST["status"];
+                        $new_data = json_encode($data,JSON_PRETTY_PRINT);
+                        if(file_put_contents("sellerData.json",$new_data)){
+                            $userBlocked = "Successful";
+                        }
+                }
+                else{
+                    $userBlocked = "Operation unsuccessful";
+                }
+            }
+        }
+    }
+            
+
+?>
